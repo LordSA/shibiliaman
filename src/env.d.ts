36084e49e18301649c47d1675b6470d88be9076f
LordSA <dlshacker159@gmail.com>
2026-06-02 19:05:13 +0530

    mass fix with lenis

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "gsap/ScrollTrigger" {
+  export const ScrollTrigger: any;
+  export default ScrollTrigger;
+}
